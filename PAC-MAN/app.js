0569be389b6dcd43c89d8bf3217051793f242d6d
Pedro Lopez <pedrolopez@pedros-mbp.home>
2020-04-17 12:00:41 +0100

    Added KeyInput

--- PAC-MAN/app.js
+++ PAC-MAN/app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded',() => {
 
-  const grid = document.querySelector('.grid')
   const scoreDisplay = document.getElementById('score')
+  const grid = document.querySelector('.grid')
   const width = 28 //28 x 28 = 784 squares
 
   //laoyout of grid and what is in the squres
-  const laoyout = [
+  const layout = [
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
@@ -35,6 +35,68 @@ document.addEventListener('DOMContentLoaded',() => {
     1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
   ]
+
+const squares = []
+
+//Legend
+//0 - pac-dot
+//1 - wall
+//2 - ghost-lair
+//3 - power-pellet
+//4 - empty
+
+//draw the grid and render it
+function createBoard() {
+  for (let i = 0; i < layout.length; i++) {
+    const square = document.createElement('div')
+    grid.appendChild(square)
+    squares.push(square)
+
+    //add layout to the board
+    if(layout[i] === 0) {
+      squares[i].classList.add('pac-dot')
+    } else if (layout[i] === 1) {
+      squares[i].classList.add('wall')
+    } else if (layout[i] === 3) {
+      squares[i].classList.add('power-pellet')
+    }
+  }
+}
+
+createBoard()
+
+//startgint position for pac-man
+let pacmanCurrentIndex = 490
+
+squares[pacmanCurrentIndex].classList.add('pac-man')
+
+//move pac-man
+function movePacman(e) {
+
+  squares[pacmanCurrentIndex].classList.remove('pac-man')
+
+  switch(e.keyCode) {
+    case 37:
+      if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
+      break
+    case 38:
+      if(pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -=width
+      break
+    case 39:
+      if(pacmanCurrentIndex % width < width -1) pacmanCurrentIndex +=1
+      break
+    case 40:
+      if(pacmanCurrentIndex + width < width * width) pacmanCurrentIndex +=width
+      break
+    }
+
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+}
+
+document.addEventListener('keyup',movePacman)
+
+
+
 
 
 })
